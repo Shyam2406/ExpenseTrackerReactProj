da694594a7a8252de2d74514e6b26b19848d3c1d
shyam patel <shyamkamaniya2406@gmail.com>
2022-04-13 21:26:53 +0530

    Personal Bill

---
+++ src/pages/PerBill.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PerBill = () => {
+  return (
+    <div>
+       <h1 className="h3 mb-3"> User's Personal Bill & Reminder </h1>
+    </div>
+  )
+}
+
+export default PerBill
